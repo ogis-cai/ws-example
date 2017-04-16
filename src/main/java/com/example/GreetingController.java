d879cb22d65804424de74703514f6b8f02df2b5e
ogis-cai <windcai10412>
2017-04-16 20:46:28 +0900

    use @EventListner replace creating new ApplicationEvent class

--- src/main/java/com/example/GreetingController.java
+++ src/main/java/com/example/GreetingController.java
@@ -2,14 +2,13 @@ package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 /**
+ *
  * Created by one on 17/04/16.
  */
 @Controller
--- src/main/java/com/example/SessionManager.java
+++ src/main/java/com/example/SessionManager.java
@@ -1,6 +1,10 @@
 package com.example;
 
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,16 +16,20 @@ import java.util.concurrent.ConcurrentHashMap;
 public class SessionManager {
     private final ConcurrentHashMap<String, SessionDto> sessionMap;
 
+    @EventListener
+    public void handleSessionConnectedEvent(SessionConnectedEvent event) {
+        StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = sha.getSessionId();
+        this.sessionMap.put(sessionId, new SessionDto(sessionId, event.getTimestamp()));
+    }
+
+    @EventListener
+    public void handleSessionDisconnectedEvent(SessionDisconnectEvent event) {
+        this.sessionMap.remove(event.getSessionId());
+    }
+
     public SessionManager() {
         this.sessionMap = new ConcurrentHashMap<>();
-    }
-
-    public void setSession(SessionDto session) {
-        this.sessionMap.put(session.getId(), session);
-    }
-
-    public void removeSession(String sessionId) {
-        this.sessionMap.remove(sessionId);
     }
 
     public Collection<SessionDto> allSessions() {
